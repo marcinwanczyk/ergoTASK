220a746f58e4bacbdeeeb366168eb040879c1329
veeexy <marasek515@gmail.com>
2024-10-10 22:31:30 +0200

    backend: build logic, sieveOfEratosthenes alg

--- src/main/java/com/prime/prime/PrimeService.java
+++ src/main/java/com/prime/prime/PrimeService.java
@@ -1,8 +1,35 @@
 package com.prime.prime;
-
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class PrimeService {
 
+    public List<Integer> checkPrime(int bound) {
+        List<Integer> primes = new ArrayList<>();
+
+        boolean[] isPrime = new boolean[bound + 1];
+
+        for(int i = 2; i <= bound; i++) {
+            isPrime[i] = true;
+        }
+
+        for(int p = 2; p * p <= bound; p++){
+            if(isPrime[p] == true){
+                for(int i = p * p; i <= bound; i += p){
+                    isPrime[i] = false;
+                }
+            }
+        }
+
+        for(int i = 2; i <= bound; i++){
+            if(isPrime[i] == true){
+                primes.add(i);
+            }
+        }
+
+        return primes;
+    }
 }
